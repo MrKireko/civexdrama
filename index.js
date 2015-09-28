15f4a3d789601bcc6c385ccbc7dc69354b6f57e1
MrKireko <mrkireko@gmail.com>
2015-09-28 19:33:14 +0200

    Boom
    launch

---
+++ index.js
@@ -0,0 +1,13 @@
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var lastDrama = new Date("2015-09-28T23:00:00.000Z");
+var today = new Date();
+
+var diffDays = Math.round(Math.abs((lastDrama.getTime() - today.getTime())/(oneDay)));
+
+console.log(lastDrama);
+console.log(today);
+console.log(diffDays);
+
+$(document).ready(function(){
+    $("#days").html(diffDays);
+});
